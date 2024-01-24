fc6e797a5afb582eb903310aa41d65036b63baf1
Noah Grosh <74073151+Septillion24@users.noreply.github.com>
2024-01-24 15:58:57 +0000

    init 2

--- Solution.cs
+++ Solution.cs
@@ -1,8 +1,14 @@
 class Solution
 {
+    static bool[][] board = new();
+    static int restarts = 0;
+
     static void main()
     {
-        
+        for (int i = 0; i < 8; i++)
+        {
+            
+        }
     }
 }
 
