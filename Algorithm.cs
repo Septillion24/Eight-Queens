a1d2dfec7ac2356bb818a1ce8a6bc4122062a64a
Septillion24 <noahgrosh@gmail.com>
2024-02-03 10:16:11 -0500

    Finished project

--- Algorithm.cs
+++ Algorithm.cs
@@ -2,22 +2,35 @@ class Algorithm
 {
     Board? currentState;
     List<Board> nextStates;
-
+    int restarts;
+    int stateChanges;
     public Algorithm()
     {
         reset();
+        restarts = 0;
+        stateChanges = 0;
+    }
+
+    public int getRestarts()
+    {
+        return restarts;
+    }
+    public int getStateChanges()
+    {
+        return stateChanges;
     }
 
     public void reset()
     {
         currentState = new();
         nextStates = new();
+        restarts++;
+        fillNextStates();
     }
 
     public void fillNextStates()
     {
         List<Vector2Int> possibleMoves = currentState.getAllPossibleMoves();
-
         foreach (Vector2Int move in possibleMoves)
         {
             Board state = currentState.getBoardAfterMovePiece(move);
@@ -25,39 +38,76 @@ class Algorithm
         }
     }
 
-    public Board? getBestStateFromNextStates()
+    public Board? getBestStateFromNextStates(out int counter)
     {
-        Board currentBest = currentState;
+        Board? currentBest = currentState;
+        counter = 0;
         foreach (Board state in nextStates)
         {
-            if(state.getConflicts() < currentBest.getConflicts())
+            if (currentBest == null)
             {
                 currentBest = state;
             }
+            else if (state.getConflicts() < currentBest.getConflicts())
+            {
+                counter = 0;
+                currentBest = state;
+            }
+            else if (state.getConflicts() == currentBest.getConflicts())
+            {
+                counter++;
+            }
         }
 
-        if(currentBest == currentState)
+        if (currentBest == currentState)
         {
+            counter = 0;
             return null;
         }
 
         return currentBest;
     }
 
-    public void doNextCalculationStep()
+    public void displayCurrentState()
     {
-        currentState = getBestStateFromNextStates();
-        if(currentState == null)
+        Console.WriteLine(currentState);
+    }
+
+
+    public bool doNextCalculationStep()
+    {
+        Console.WriteLine("Current h: " + currentState.getConflicts());
+        Console.WriteLine(currentState.ToString());
+        int counter = 0;
+        Board? betterState = getBestStateFromNextStates(out counter);
+        currentState = betterState;
+        stateChanges++;
+        Console.WriteLine("Neighbors found with better h: " + counter);
+
+        if (currentState == null)
         {
-            //TODO
+            Console.WriteLine("Restart.");
             reset();
         }
         else
         {
+            Console.WriteLine("\nGetting better state.");
             fillNextStates();
-            currentState = ;
         }
 
+        return checkWinningState();
+    }
+
+    public bool checkWinningState()
+    {
+        if (currentState.getConflicts() == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
 
--- Program.cs
+++ Program.cs
@@ -5,6 +5,20 @@ class Program
 
     static void Main()
     {
-        
+        Algorithm algorithm = new();
+        // algorithm.displayCurrentState();
+
+        bool win = false;
+        do
+        {
+            win = algorithm.doNextCalculationStep();
+        }
+        while (!win);
+        Console.WriteLine("\n\nFound final state.");
+        algorithm.displayCurrentState();
+        Console.WriteLine("\nState changes: " + algorithm.getStateChanges() + "\nRestarts: " + algorithm.getRestarts());
+
+
+
     }
 }
